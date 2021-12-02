fbc9779ad5972b3fadec703806a2aac08691cbbb
Hai-Nam Nguyen <hainam@jcisio.com>
2021-12-02 22:04:32 +0100

    Day 02 Part 2

--- day02/d02.py
+++ day02/d02.py
@@ -16,5 +16,23 @@ def findResult(lines):
     return position * depth
 
 
+def findResult2(lines):
+    position = 0
+    depth = 0
+    aim = 0
+    for line in lines:
+        (direction, amount) = line.split()
+        amount = int(amount)
+        if direction == 'down':
+            aim += amount
+        elif direction == 'up':
+            aim -= amount
+        elif direction == 'forward':
+            position += amount
+            depth += aim * amount
+    return position * depth
+
+
 lines = f.read().strip().split('\n')
 print("Puzzle 1: ", findResult(lines))
+print("Puzzle 2: ", findResult2(lines))
