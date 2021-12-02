69e74fe264cf7d26c5076da0a6821ebb8d2be1eb
Hai-Nam Nguyen <hainam@jcisio.com>
2021-12-02 21:42:05 +0100

    Day 01 Part 2

--- day01/d01.py
+++ day01/d01.py
@@ -1,9 +1,10 @@
 f = open('d01.in', 'r')
 
 
-def findMeasurementIncreases(lines):
-    return sum(1 if lines[i] < lines[i+1] else 0 for i in range(len(lines)-1))
+def findMeasurementIncreases(lines, window_size = 1):
+    return sum(1 if lines[i] < lines[i+window_size] else 0 for i in range(len(lines)-window_size))
 
 
 lines = list(map(int, f.read().strip().split('\n')))
 print("Puzzle 1: ", findMeasurementIncreases(lines))
+print("Puzzle 2: ", findMeasurementIncreases(lines, 3))
