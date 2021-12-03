002ffb3d5d2073d4f7fed83562aa94dfeb15a4e2
Hai-Nam Nguyen <hainam@jcisio.com>
2021-12-03 21:13:05 +0100

    Day 03 Part 2

--- day03/d03.py
+++ day03/d03.py
@@ -10,5 +10,23 @@ def findPowerConsumption(lines):
         epsilon = epsilon*2 + (1 - major_bit)
     return gamma * epsilon
 
+def filterByBitCriteira(lines, bit):
+    i = 0
+    elements = lines.copy()
+    while len(elements) > 1:
+        nbits = sum([int(element[i]) for element in elements])
+        if nbits < len(elements)/2:
+            major_bit = 1 - bit
+        else:
+            major_bit = bit
+        major_bit = str(major_bit)
+        elements = [element for element in elements if element[i] == major_bit]
+        i += 1
+    return int(elements[0], 2)
+
+def findLifeSupportRating(lines):
+    return filterByBitCriteira(lines, 1) * filterByBitCriteira(lines, 0)
+
 lines = f.read().strip().split('\n')
 print("Puzzle 1: ", findPowerConsumption(lines))
+print("Puzzle 2: ", findLifeSupportRating(lines))
