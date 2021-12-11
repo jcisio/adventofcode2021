980b1cd4ac7deaaf5f21b1d67702c9d53c41743d
Hai-Nam Nguyen <hainam@jcisio.com>
2021-12-11 22:36:47 +0100

    Day 11

--- clone.sh
+++ clone.sh
@@ -2,6 +2,6 @@
 cp -r day01 day$1
 cd day$1
 rename s/01/$1/ *.*
-curl "https://adventofcode.com/2021/day/10/input" -H "cookie: session=$AOC2021SID" > d$1.in
+curl "https://adventofcode.com/2021/day/$1/input" -H "cookie: session=$AOC2021SID" > d$1.in
 echo -n > problem.md
 git add d$1.in problem.md
