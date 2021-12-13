91a39810cec410fd0d0a80f9f1bfc1b9c976b737
Hai-Nam Nguyen <hainam@jcisio.com>
2021-12-13 22:29:33 +0100

    Day 12 Part 2

--- day12/d12.py
+++ day12/d12.py
@@ -21,20 +21,25 @@ class Problem:
     def solve(self):
         self.readInput()
         print("Puzzle 1: ", self.solvePart1())
+        print("Puzzle 2: ", self.solvePart2())
 
-    def findNextCave(self, current_path):
+    def findNextCave(self, current_path, revisit=False):
         found = []
         for cave in self.caves:
             if (current_path[-1], cave) not in self.edges:
                 continue
             if cave == 'end':
                 found.append(current_path + [cave])
-            elif cave not in current_path or self.caves[cave]:
-                found += self.findNextCave(current_path + [cave])
+            elif (cave not in current_path) or self.caves[cave] or (revisit and cave != 'start'):
+                can_revisit = (cave not in current_path or self.caves[cave]) and revisit
+                found += self.findNextCave(current_path + [cave], can_revisit)
         return found
 
     def solvePart1(self):
         return len(self.findNextCave(['start']))
+
+    def solvePart2(self):
+        return len(self.findNextCave(['start'], True))
 
 
 problem = Problem()
