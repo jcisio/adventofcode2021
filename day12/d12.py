fdedb57147356410872c3c4f82778a4bd1e346ab
Hai-Nam Nguyen <hainam@jcisio.com>
2021-12-13 22:18:54 +0100

    Day 12 Part 1

---
+++ day12/d12.py
@@ -0,0 +1,41 @@
+
+class Problem:
+
+    def __init__(self):
+        # List of caves, True = big cave, False = small cave
+        self.caves = {}
+        # List of edge between two caves.
+        self.edges = {}
+
+    def readInput(self):
+        f = open(__file__[:-3] + '.in', 'r')
+        for line in f.read().strip().split('\n'):
+            s, e = line.split('-')
+            self.edges[(s, e)] = 1
+            self.edges[(e, s)] = 1
+            if s not in self.caves:
+                self.caves[s] = (s == s.upper())
+            if e not in self.caves:
+                self.caves[e] = (e == e.upper())
+
+    def solve(self):
+        self.readInput()
+        print("Puzzle 1: ", self.solvePart1())
+
+    def findNextCave(self, current_path):
+        found = []
+        for cave in self.caves:
+            if (current_path[-1], cave) not in self.edges:
+                continue
+            if cave == 'end':
+                found.append(current_path + [cave])
+            elif cave not in current_path or self.caves[cave]:
+                found += self.findNextCave(current_path + [cave])
+        return found
+
+    def solvePart1(self):
+        return len(self.findNextCave(['start']))
+
+
+problem = Problem()
+problem.solve()
