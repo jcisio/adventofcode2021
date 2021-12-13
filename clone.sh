465868db85cd5d01937dc371163b07127843335c
Hai-Nam Nguyen <hainam@jcisio.com>
2021-12-13 22:31:20 +0100

    Use day12 as base for copy

--- clone.sh
+++ clone.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-cp -r day01 day$1
+cp -r day12 day$1
 cd day$1
-rename s/01/$1/ *.*
+rename s/12/$1/ *.*
 curl "https://adventofcode.com/2021/day/$1/input" -H "cookie: session=$AOC2021SID" > d$1.in
 echo -n > problem.md
 git add d$1.in problem.md
