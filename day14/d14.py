22cb32db91c285baba851f44916390376727e353
Hai-Nam Nguyen <hainam@jcisio.com>
2021-12-15 22:40:09 +0100

    Day 14 Part 2

--- day14/d14.py
+++ day14/d14.py
@@ -6,6 +6,7 @@ class Problem:
     def __init__(self):
         self.template = ''
         self.rules = {}
+        self.pairs = {}
 
     def readInput(self):
         f = open(__file__[:-3] + '.in', 'r')
@@ -18,6 +19,7 @@ class Problem:
     def solve(self):
         self.readInput()
         print("Puzzle 1: ", self.solvePart1())
+        print("Puzzle 2: ", self.solvePart2())
 
     def doStep(self, template):
         new_template = template[0]
@@ -27,12 +29,33 @@ class Problem:
             new_template += template[i+1]
         return new_template
 
+    def doStepFaster(self, pairs):
+        new_pairs = {}
+        for k, v in pairs.items():
+            pair1 = k[0] + self.rules[k]
+            pair2 = self.rules[k] + k[1]
+            new_pairs[pair1] = (new_pairs[pair1] if pair1 in new_pairs else 0) + v
+            new_pairs[pair2] = (new_pairs[pair2] if pair2 in new_pairs else 0) + v
+        return new_pairs
+
     def solvePart1(self):
         template = self.template
         for i in range(10):
             template = self.doStep(template)
+            counter = Counter(template)
         counter = Counter(template)
         return max(counter.values()) - min(counter.values())
+
+    def solvePart2(self):
+        counter = Counter(self.template[i:i+2] for i in range(len(self.template) - 1))
+        pairs = {k: v for k, v in counter.items()}
+        for i in range(40):
+            pairs = self.doStepFaster(pairs)
+        counter = {self.template[0]: 1, self.template[-1]: 1}
+        for k, v in pairs.items():
+            counter[k[0]] = (counter[k[0]] if k[0] in counter else 0) + v
+            counter[k[1]] = (counter[k[1]] if k[1] in counter else 0) + v
+        return (max(counter.values()) - min(counter.values()))//2
 
 problem = Problem()
 problem.solve()
