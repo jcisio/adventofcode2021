c0d5d565fa3aed3e64d540f01bf33b7a0ab0752d
Hai-Nam Nguyen <hainam@jcisio.com>
2021-12-17 22:27:29 +0100

    Day 17 Part 2

--- day17/d17.py
+++ day17/d17.py
@@ -11,6 +11,7 @@ class Problem:
     def solve(self):
         self.readInput()
         print("Puzzle 1: ", self.solvePart1())
+        print("Puzzle 2: ", self.solvePart2())
 
     def findVx(self):
         for vx in range(1, self.target[1] + 1):
@@ -29,6 +30,25 @@ class Problem:
         vx = self.findVx()
         vy = -1 - self.target[2]
         return vy*(vy+1)//2
+
+    def testVelocity(self, vx, vy):
+        x, y = 0, 0
+        while ((vx > 0 and x < self.target[1]) or (vx == 0 and self.target[0] <= x <= self.target[1])) and (y >= self.target[2]):
+            x += vx
+            y += vy
+            if self.target[0] <= x <= self.target[1] and self.target[2] <= y <= self.target[3]:
+                return 1
+            vx = max(vx - 1, 0)
+            vy = vy - 1
+        return 0
+
+    def solvePart2(self):
+        vx = self.findVx()
+        vy = -1 - self.target[2]
+        min_vy = self.target[2]
+        max_vx = self.target[1]
+        return sum(sum(self.testVelocity(vx, y) for y in range(min_vy, vy+1)) for vx in range(vx, max_vx+1))
+
 
 problem = Problem()
 problem.solve()
